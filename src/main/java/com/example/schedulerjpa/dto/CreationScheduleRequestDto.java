54c0c63777327b5110e761a763a62185563d964b
prem3dev <ecstasy1508@gmail.com>
2025-03-31 19:12:45 +0900

    :adhesive_bandage: CreationScheduleRequestDto Rename

--- src/main/java/com/example/schedulerjpa/dto/CreationScheduleRequestDto.java
+++ src/main/java/com/example/schedulerjpa/dto/CreationScheduleRequestDto.java
@@ -5,11 +5,12 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class CreationRequestDto {
+public class CreationScheduleRequestDto {
 
     private final String authorName;
 
     private final String title;
 
     private final String task;
+
 }
