2998fec5248baf99f4d5736af46c5a0bd9c94a43
prem3dev <ecstasy1508@gmail.com>
2025-04-01 16:05:58 +0900

    :adhesive_bandage: 안쓰는 import 정리

--- src/main/java/com/example/schedulerjpa/service/ScheduleServiceImpl.java
+++ src/main/java/com/example/schedulerjpa/service/ScheduleServiceImpl.java
@@ -7,12 +7,8 @@ import com.example.schedulerjpa.dto.UpdateScheduleResponseDto;
 import com.example.schedulerjpa.entity.Schedule;
 import com.example.schedulerjpa.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
