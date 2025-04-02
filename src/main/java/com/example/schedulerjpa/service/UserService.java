d0c3eb2a1d2686dceec838ac16a93526d3c2bf9a
prem3dev <ecstasy1508@gmail.com>
2025-04-02 12:35:09 +0900

    :sparkles: CRUD 기능 구현

--- src/main/java/com/example/schedulerjpa/service/UserService.java
+++ src/main/java/com/example/schedulerjpa/service/UserService.java
@@ -1,8 +1,23 @@
 package com.example.schedulerjpa.service;
 
+import com.example.schedulerjpa.dto.SearchUserResponseDto;
 import com.example.schedulerjpa.dto.SignUpUserResponseDto;
+import com.example.schedulerjpa.dto.UpdateUserRequestDto;
+import com.example.schedulerjpa.dto.UpdateUserResponseDto;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatusCode;
+
+import java.util.List;
 
 public interface UserService {
 
     SignUpUserResponseDto signUpUser(String userName, String email);
+
+    List<SearchUserResponseDto> findAllUsers();
+
+    SearchUserResponseDto findUserById(Long id);
+
+    UpdateUserResponseDto updateUser(Long id, UpdateUserRequestDto requestDto);
+
+    void deleteUserById(@NotNull Long id);
 }
--- src/main/java/com/example/schedulerjpa/service/UserServiceImpl.java
+++ src/main/java/com/example/schedulerjpa/service/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package com.example.schedulerjpa.service;
 
+import com.example.schedulerjpa.dto.SearchUserResponseDto;
 import com.example.schedulerjpa.dto.SignUpUserResponseDto;
+import com.example.schedulerjpa.dto.UpdateUserRequestDto;
+import com.example.schedulerjpa.dto.UpdateUserResponseDto;
 import com.example.schedulerjpa.entity.User;
 import com.example.schedulerjpa.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -25,5 +33,43 @@ public class UserServiceImpl implements UserService{
                 savedUser.getEmail(),
                 savedUser.getCreatedAt(),
                 savedUser.getModifiedAt());
+    }
+
+    @Override
+    public List<SearchUserResponseDto> findAllUsers() {
+        List<User> userList = userRepository.findAll();
+        if(userList.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return userList.stream().map(SearchUserResponseDto::new).toList();
+    }
+
+    @Override
+    public SearchUserResponseDto findUserById(Long id) {
+        User user = userRepository.findUserByIdOrElseThrow(id);
+        return new SearchUserResponseDto(user);
+    }
+
+    @Transactional
+    @Override
+    public UpdateUserResponseDto updateUser(Long id, UpdateUserRequestDto requestDto) {
+        User user = userRepository.findUserByIdOrElseThrow(id);
+
+        if (requestDto.getUserName() != null && !requestDto.getUserName().isBlank()) {
+            user.setUserName(requestDto.getUserName());
+        }
+        if(requestDto.getEmail() != null && !requestDto.getEmail().isBlank()) {
+            user.setEmail(requestDto.getEmail());
+        }
+
+        User newUser = userRepository.findUserByIdOrElseThrow(id);
+        return new UpdateUserResponseDto(newUser);
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+        User user = userRepository.findUserByIdOrElseThrow(id);
+
+        userRepository.delete(user);
     }
 }
