a03c8f5887d437ea5aeeda991818f6772c5738d8
prem3dev <ecstasy1508@gmail.com>
2025-04-03 23:22:26 +0900

    :recycle: email로 User Entity를 조회해서 스케줄 Entity 생성할 때 연관관계를 설정하였던 것을 로그인 상태의 세션을 통해 설정하도록 변경

--- src/main/java/com/example/schedulerjpa/service/ScheduleService.java
+++ src/main/java/com/example/schedulerjpa/service/ScheduleService.java
@@ -1,21 +1,21 @@
 package com.example.schedulerjpa.service;
 
-import com.example.schedulerjpa.dto.CreationScheduleResponseDto;
-import com.example.schedulerjpa.dto.SearchScheduleResponseDto;
-import com.example.schedulerjpa.dto.UpdateScheduleRequestDto;
-import com.example.schedulerjpa.dto.UpdateScheduleResponseDto;
+import com.example.schedulerjpa.dto.scheduledto.CreationScheduleResponseDto;
+import com.example.schedulerjpa.dto.scheduledto.SearchScheduleResponseDto;
+import com.example.schedulerjpa.dto.scheduledto.UpdateScheduleRequestDto;
+import com.example.schedulerjpa.dto.scheduledto.UpdateScheduleResponseDto;
 
 import java.util.List;
 
 public interface ScheduleService {
 
-    public CreationScheduleResponseDto createSchedule(String email, String title, String task);
+    public CreationScheduleResponseDto createSchedule(Long id, String title, String task);
 
     public List<SearchScheduleResponseDto> findAllSchedules();
 
     public SearchScheduleResponseDto findScheduleById(Long id);
 
-    public UpdateScheduleResponseDto updateSchedule(Long id, UpdateScheduleRequestDto requestDto);
+    public UpdateScheduleResponseDto updateSchedule(Long id, UpdateScheduleRequestDto requestDto, Long loginUserId);
 
-   public void deleteSchedule(Long id);
+   public void deleteSchedule(Long id, Long loginUserId);
 }
