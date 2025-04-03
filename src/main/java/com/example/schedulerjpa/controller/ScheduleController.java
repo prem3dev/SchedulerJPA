8fd1579158c1580c02c4feb44fb01cc7c45473f3
prem3dev <ecstasy1508@gmail.com>
2025-04-03 23:20:27 +0900

    :recycle: email로 User Entity를 조회해서 스케줄 Entity 생성할 때 연관관계를 설정하였던 것을 로그인 상태의 세션을 통해 설정하도록 변경

--- src/main/java/com/example/schedulerjpa/controller/ScheduleController.java
+++ src/main/java/com/example/schedulerjpa/controller/ScheduleController.java
@@ -1,6 +1,7 @@
 package com.example.schedulerjpa.controller;
 
-import com.example.schedulerjpa.dto.*;
+import com.example.schedulerjpa.common.Const;
+import com.example.schedulerjpa.dto.scheduledto.*;
 import com.example.schedulerjpa.service.ScheduleService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -21,19 +22,17 @@ public class ScheduleController {
 
     //일정 생성
     @PostMapping
-    public ResponseEntity<CreationScheduleResponseDto> createSchedule(@RequestBody @Valid CreationScheduleRequestDto requestDto) {
-
+    public ResponseEntity<CreationScheduleResponseDto> createSchedule(
+            @SessionAttribute(name = Const.LOGIN_USER) Long id,
+            @RequestBody @Valid CreationScheduleRequestDto requestDto) {
         CreationScheduleResponseDto responseDto =
-                scheduleService.createSchedule(
-                        requestDto.getEmail(),
-                        requestDto.getTitle(),
-                        requestDto.getTask()
+                scheduleService.createSchedule(id, requestDto.getTitle(), requestDto.getTask()
                 );
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
     //전체 일정 조회
-    @GetMapping
+    @GetMapping("/total")
     public ResponseEntity<List<SearchScheduleResponseDto>> findAllSchedules() {
         return new ResponseEntity<>(scheduleService.findAllSchedules(), HttpStatus.OK);
     }
@@ -50,18 +49,20 @@ public class ScheduleController {
     @Validated
     @PatchMapping("/individual/{id}")
     public ResponseEntity<UpdateScheduleResponseDto> updateSchedule(
-            @PathVariable @NotNull Long id, @RequestBody @Valid UpdateScheduleRequestDto requestDto) {
+            @PathVariable @NotNull Long id,
+            @RequestBody @Valid UpdateScheduleRequestDto requestDto,
+            @SessionAttribute(name = Const.LOGIN_USER, required = false) Long loginUserId) {
 
-      return new ResponseEntity<>(scheduleService.updateSchedule(id, requestDto), HttpStatus.OK);
+      return new ResponseEntity<>(scheduleService.updateSchedule(id, requestDto, loginUserId), HttpStatus.OK);
     }
 
     //개별 일정 삭제
     @Validated
     @DeleteMapping("/individual/{id}")
-    public ResponseEntity<Void> deleteSchedule (@PathVariable @NotNull Long id) {
-
-        scheduleService.deleteSchedule(id);
-
+    public ResponseEntity<Void> deleteSchedule (
+            @PathVariable @NotNull Long id,
+            @SessionAttribute(name = Const.LOGIN_USER, required = false) Long loginUserId) {
+        scheduleService.deleteSchedule(id, loginUserId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
