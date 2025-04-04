bc2a92d11e1e165c6149c65c60a5bb098efbe909
prem3dev <ecstasy1508@gmail.com>
2025-04-04 13:57:19 +0900

    :recycle: 현재 비밀번호를 필수로 요청 받게끔 수정

--- src/main/java/com/example/schedulerjpa/dto/userdto/UpdateUserRequestDto.java
+++ src/main/java/com/example/schedulerjpa/dto/userdto/UpdateUserRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.schedulerjpa.dto.userdto;
 
 import com.example.schedulerjpa.customannotation.NotAllNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,6 +14,7 @@ public class UpdateUserRequestDto {
 
     private final String email;
 
+    @NotBlank
     private final String presentPassword;
 
     private final String newPassword;
